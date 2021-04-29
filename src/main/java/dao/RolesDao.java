d5d93e5d2708e0251b91682d89f4da252fab7e6f
t <t@gmail.com>
2021-04-29 20:01:24 +0200

    Funcionalidad CrearRol acabada y metido plantilla html y css para los formularios

--- src/main/java/dao/RolesDao.java
+++ src/main/java/dao/RolesDao.java
@@ -5,9 +5,10 @@ import java.util.List;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.hibernate.Session;
-
+import org.hibernate.Transaction;
 
 import main.java.entities.Roles;
+import main.java.entities.Usuarios;
 
 public class RolesDao {
 
@@ -29,5 +30,26 @@ public class RolesDao {
 		String hql = "from Roles";
 		lista = s.createQuery(hql, Roles.class).list();
 		return lista;
+	}
+	
+	public static boolean insertarRol(Session s, int id, String nombre) {
+		boolean correcto = false;
+		Transaction t = s.beginTransaction();
+		try {
+			Roles r = new Roles(id, nombre);
+			if(!RolesDao.consultarRol(s, id)) {
+				s.save(r);
+				t.commit();
+				logger.info("Rol creado");
+				correcto = true;
+			}else {
+				logger.warn("El rol ya existe");
+			}
+		}catch(Exception ex) {
+			logger.error(ex);
+			t.rollback();
+		}
+		s.close();
+		return correcto;
 	}
 }
